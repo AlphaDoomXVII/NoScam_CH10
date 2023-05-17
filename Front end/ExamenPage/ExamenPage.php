37ed1589e1dba9f556a3b6e169f855b04866ce17
AlphaDoomXVII <514345@vistacollege.nl>
2023-05-17 10:02:52 +0200

    Basis front end op alle pagina's

---
+++ Front end/ExamenPage/ExamenPage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NoScam | Home page</title>
+
+    <link rel="icon" href="../Assets/NoScam.png">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+    <header> <img src="../Assets/NoScam.png" id="NoScam">  
+        <button id="HomeBtn" class="HeaderBtns" >Home</button>
+        <button id="InfoBtn" class="HeaderBtns" >Info</button>
+        <button id="TestenBtn" class="HeaderBtns" >Testen</button>
+        <button id="CursussenBtn" class="HeaderBtns" >Cursussen</button>
+        <button id="ExamenBtn" class="HeaderBtns" >Examen</button>
+    </header>
+
+
+
+
+
+</body>
+</html>
